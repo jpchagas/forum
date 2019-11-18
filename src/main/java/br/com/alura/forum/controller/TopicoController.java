6864d259f39bc00256241ccb7fcabf7f4b6fa33b
João Pedro Chagas <jpchagas@gmail.com>
2019-11-18 17:29:59 -0300

    Acessando o banco de dados e incluindo padrão repository

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -4,6 +4,8 @@ import br.com.alura.forum.dto.TopicoDTO;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +16,13 @@ import java.util.List;
 @RestController
 public class TopicoController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDTO> lista(){
-        Topico topico = new Topico("Dúvida", "Duvida com spring", new Curso("Srping", "Programação"));
-        return TopicoDTO.converter(Arrays.asList(topico,topico,topico));
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDTO.converter(topicos);
     }
 
 }
